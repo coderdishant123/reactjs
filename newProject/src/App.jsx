7ce1c5282b26f8348498afbea6069f0767b4a304
dishantsharma <sharmadeeshu00@gmail.com>
2025-10-30 10:27:36 +0530

    react file upload

---
+++ newProject/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/footer'
+
+import Title from './components/Title'
+
+function App() {
+
+  return (
+    <>
+      <Title/>
+      <Navbar/>
+      <Footer/>
+     
+    </>
+  )
+}
+
+export default App;
---
+++ newProject/src/components/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>
+        <p>
+            hey dishant this side
+        </p>
+
+      
+    </div>
+  )
+}
+
+export default Title
